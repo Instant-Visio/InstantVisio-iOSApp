f61c2a02165a25779b1be3473da5a2bd91849f0f
DOMINIQUE BERNARD Kanga <kangadominiquebernard@gmail.com>
2020-03-30 22:35:02 +0200

    set config firebase by env file

--- src-ui/global/firebase/firebase.js
+++ src-ui/global/firebase/firebase.js
@@ -1,5 +1,5 @@
 import Config from './../config';
-import {Alert,Platform} from 'react-native';
+import {Platform} from 'react-native';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/functions';
 import '@react-native-firebase/auth';
@@ -9,34 +9,33 @@ import '@react-native-firebase/analytics';
 
 //pluck values from your 'google-services.json' you created on the firebase console
 const androidConfig = {
-    clientId: '1069540149829-dmnal5i765t4gqgn0vgc0dd4777p8501.apps.googleusercontent.com',
-    appId:'1:1069540149829:android:203b385ac1bd8eb245f12c',
-    apiKey: 'AIzaSyDEJ9XcxNxwujAB5c_hyoK1Gln-XTBdLq4',
-    authDomain: Config.REACT_APP_AUTH_DOMAIN,
-    databaseURL: 'https://flash-antler-244109.firebaseio.com/',
-    projectId: 'flash-antler-244109',
-    messagingSenderId:'1069540149829',
-    storageBucket:'gs://flash-antler-244109.appspot.com',
-    measurementId: Config.REACT_APP_MEASUREMENT_ID,
-    persistence: true,
-    }
+    clientId: Config.REACT_NATIVE_APP_CLIENTID,
+    appId: Config.REACT_NATIVE_APP_APPID,
+    apiKey: Config.REACT_NATIVE_APP_API_KEY,
+    authDomain: Config.REACT_NATIVE_APP_AUTH_DOMAIN,
+    databaseURL: Config.REACT_NATIVE_APP_DATABASE_URL,
+    projectId: Config.REACT_NATIVE_APP_PROJECT_ID,
+    messagingSenderId: Config.REACT_NATIVE_APP_MESSAGINGSENDERID,
+    storageBucket: Config.REACT_NATIVE_APP_STORAGEBUCKET,
+    measurementId: Config.REACT_NATIVE_APP_MEASUREMENT_ID,
+    persistence: Config.REACT_NATIVE_APP_PERSISTENCE
+}
 
 
 // pluck values from your `GoogleService-Info.plist` you created on the firebase console
 const iosConfig = {
-    clientId: '1069540149829-dmnal5i765t4gqgn0vgc0dd4777p8501.apps.googleusercontent.com',
-    appId:'1:1069540149829:android:203b385ac1bd8eb245f12c',
-    apiKey: 'AIzaSyDEJ9XcxNxwujAB5c_hyoK1Gln-XTBdLq4',
-    authDomain: Config.REACT_APP_AUTH_DOMAIN,
-    databaseURL: 'https://flash-antler-244109.firebaseio.com/',
-    projectId: 'flash-antler-244109',
-    messagingSenderId:'1069540149829',
-    storageBucket:'gs://flash-antler-244109.appspot.com',
-    measurementId: Config.REACT_APP_MEASUREMENT_ID,
-    persistence: true,
-    }
+    clientId: Config.REACT_NATIVE_APP_CLIENTID,
+    appId: Config.REACT_NATIVE_APP_APPID,
+    apiKey: Config.REACT_NATIVE_APP_API_KEY,
+    authDomain: Config.REACT_NATIVE_APP_AUTH_DOMAIN,
+    databaseURL: Config.REACT_NATIVE_APP_DATABASE_URL,
+    projectId: Config.REACT_NATIVE_APP_PROJECT_ID,
+    messagingSenderId: Config.REACT_NATIVE_APP_MESSAGINGSENDERID,
+    storageBucket: Config.REACT_NATIVE_APP_STORAGEBUCKET,
+    measurementId: Config.REACT_NATIVE_APP_MEASUREMENT_ID,
+    persistence: Config.REACT_NATIVE_APP_PERSISTENCE
+}
 
-    Alert.alert(Config.REACT_NATIVE_APP_APPID);
 
 //inside function check before initializing
 firebase.apps.length!==0 ? firebase.app() : firebase.initializeApp(
@@ -44,8 +43,6 @@ firebase.apps.length!==0 ? firebase.app() : firebase.initializeApp(
 Platform.OS === 'ios' ? iosConfig : androidConfig
 );
 
-
-// firebase.initializeApp(firebaseConfig)
 
 export const remoteConfig = firebase.remoteConfig()
 remoteConfig.settings = {
