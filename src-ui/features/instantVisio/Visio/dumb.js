196c05d49e18a1fb68b0e5560cb525cec712a76c
DOMINIQUE BERNARD Kanga <kangadominiquebernard@gmail.com>
2020-05-06 04:55:58 +0200

    Build a RN app version that is linked to `dev` webApp    Closes:#37

--- src-ui/features/instantVisio/Visio/dumb.js
+++ src-ui/features/instantVisio/Visio/dumb.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import WebView from 'react-native-webview';
+import Config from 'react-native-config';
 
 const Dump = ({navigation, videoCallIdProps}) => (
   <>
     {/** tu peux l'utiliser ici egalement**/}
     <WebView
       style={{flex:1}}
-      source={{uri: `https://instantvisio.com/visio/${videoCallIdProps}`}}
+      source={{uri: `${Config.URL_CALL}${videoCallIdProps}`}}
       /* Does not store any data within the lifetime of the WebView. */
       // incognito={true}
     />
