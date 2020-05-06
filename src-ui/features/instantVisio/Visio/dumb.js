e6ed7befafa292341aacb97f45c74249407822df
Mattia Primavera <sconqua@gmail.com>
2020-05-06 19:50:19 +0200

    Use instant-visio call url instead of webApp
      #39

--- src-ui/features/instantVisio/Visio/dumb.js
+++ src-ui/features/instantVisio/Visio/dumb.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import WebView from 'react-native-webview';
-import DeviceInfo from 'react-native-device-info';
 
 const Dump = ({navigation, videoCallIdProps}) => (
   <>
     {/** tu peux l'utiliser ici egalement**/}
     <WebView
       style={{flex:1}}
-      source={{uri: `${DeviceInfo.getBrand().toUpperCase()==='SAMSUNG'?'https://instantvisio.daily.co/':'https://instantvisio.com/visio/'}${videoCallIdProps}`}}
+      source={{uri: `https://instantvisio.daily.co/${videoCallIdProps}`}}
       /* Does not store any data within the lifetime of the WebView. */
       // incognito={true}
     />
