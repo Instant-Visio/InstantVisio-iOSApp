ec65a846dfc1d3a7529664d912dd55b00419ab77
DOMINIQUE BERNARD Kanga <kangadominiquebernard@gmail.com>
2020-05-06 18:47:10 +0200

    Change visio url for samsung devices #38 in master

--- src-ui/features/instantVisio/Visio/dumb.js
+++ src-ui/features/instantVisio/Visio/dumb.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import WebView from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
 
 const Dump = ({navigation, videoCallIdProps}) => (
   <>
     {/** tu peux l'utiliser ici egalement**/}
     <WebView
       style={{flex:1}}
-      source={{uri: `https://instantvisio.com/visio/${videoCallIdProps}`}}
+      source={{uri: `${DeviceInfo.getBrand().toUpperCase()==='SUMSUNG'?'https://instantvisio.daily.co/':'https://instantvisio.com/visio/'}${videoCallIdProps}`}}
       /* Does not store any data within the lifetime of the WebView. */
       // incognito={true}
     />
