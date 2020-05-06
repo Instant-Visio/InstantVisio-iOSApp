3fbc6c8206da3135d361363ee4204cf43db61b8b
DOMINIQUE BERNARD Kanga <kangadominiquebernard@gmail.com>
2020-05-06 19:20:25 +0200

    change SUMSUNG by SAMSUNG

--- src-ui/features/instantVisio/Visio/dumb.js
+++ src-ui/features/instantVisio/Visio/dumb.js
@@ -7,7 +7,7 @@ const Dump = ({navigation, videoCallIdProps}) => (
     {/** tu peux l'utiliser ici egalement**/}
     <WebView
       style={{flex:1}}
-      source={{uri: `${DeviceInfo.getBrand().toUpperCase()==='SUMSUNG'?'https://instantvisio.daily.co/':'https://instantvisio.com/visio/'}${videoCallIdProps}`}}
+      source={{uri: `${DeviceInfo.getBrand().toUpperCase()==='SAMSUNG'?'https://instantvisio.daily.co/':'https://instantvisio.com/visio/'}${videoCallIdProps}`}}
       /* Does not store any data within the lifetime of the WebView. */
       // incognito={true}
     />
